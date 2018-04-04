67056fd3fb184e2fe5d08925ba7ba6d38527e58e
Unknown <Frederik.Schlemmer@adesso.de>
2018-04-04 13:23:22 +0200

    Adds sorting according to the commitAmount to activeUser

--- src/postRequest.ts
+++ src/postRequest.ts
@@ -36,15 +36,17 @@ export abstract class postRequest {
                 var activeUser: ActiveUser | null = functiontofindIndexByKeyValue(Organization.activeUsers, "login", response.user.login);
                 if (activeUser != null) {
                     activeUser.setCommitAmount(commitAmount)
-
                 }
+                sortActiveUsers(Organization.activeUsers);
                 break;
+
             case CrawlOrganization.SearchMostTop10ActiveUserInformation:
                 var activeUsers: Array<ActiveUser> = new Array<ActiveUser>();
                 for (let userInfo of response.organization.members.nodes) {
                     activeUsers.push(new ActiveUser(userInfo.name, userInfo.login, userInfo.id, 0));
                 }
                 return activeUsers;
+
             case CrawlOrganization.SearchMostTop10StarRepos:
                 var starredRepositories: Array<StarredRespository> = new Array<StarredRespository>();
                 for (let repoInfo of response.search.edges) {
@@ -68,6 +70,21 @@ export abstract class postRequest {
 
 function sortActiveRepositories(activeRespositories: Array<ActiveRespository>) {
     activeRespositories.sort((a, b) => {
+        if (a.getCommitAmount() == b.getCommitAmount()) {
+            return 0;
+        } else {
+            if (a.getCommitAmount() > b.getCommitAmount()) {
+                return -1;
+            }
+            else if (a.getCommitAmount() < b.getCommitAmount()) {
+                return 1;
+            }
+        }
+    })
+}
+
+function sortActiveUsers(activeUsers: Array<ActiveUser>) {
+    activeUsers.sort((a, b) => {
         if (a.getCommitAmount() == b.getCommitAmount()) {
             return 0;
         } else {
